ea6ee99dbfbffe3c3a1f1b1d7e792de7833e8e7d
Ruin0verall <96787822+Ruin0verall@users.noreply.github.com>
2025-03-14 00:54:28 +0500

    Revert "s"
    This reverts commit ec59cc202bc390d335949c8a664c9edf415d0401.

--- index.js
+++ index.js
@@ -8,13 +8,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: [
-      process.env.FRONTEND_URL || "http://localhost:5173",
-      "https://gaafu-magazine-test-eight.vercel.app",
-    ],
+    origin: process.env.FRONTEND_URL || "*",
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
